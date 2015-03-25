09a1f683f5e5fdbd03a8aad05248fae1060c4948
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2015-03-25 02:39:16 -0500

    First working draft.

---
+++ User_Advertising_Preferences.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name        User Advertising Preferences
+// @namespace   http://userscripts.org
+// @description Tell advertisers what you want.
+// @include     http://*
+// @include     https://*
+// @include     file://*
+// @exclude     chrome://*
+// @version     0.1
+// @grant       none
+// @run-at document-start
+// ==/UserScript==
+
+navigator.userAdPrefs =
+{
+    "yes" : [
+        "Mad Men",
+        "Burma-Shave",
+        "Murder Must Advertise",
+        "Confessions of an Advertising Man",
+        "How to Sell Anything to Anybody",
+        "The Man Who Sold America",
+        "Mary Wells Lawrence",
+        "Jean Wade Rindlaub",
+    ],
+    "no" : [
+        "Then We Came to the End",
+        "The Hidden Persuaders",
+        "Marlboro Man",
+    ],
+}
